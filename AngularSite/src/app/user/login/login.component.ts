f42a8e31cb4819a0640c00ae9aa21eef50d5383c
VyTale <mrdancinwolf@hotmail.com>
2019-09-16 19:56:47 -0400

    Follow-up Login

--- AngularSite/src/app/user/login/login.component.ts
+++ AngularSite/src/app/user/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, Form } from '@angular/forms';
+import { Utilisateur } from 'src/app/shared/utilisateur.model';
+import { UtilisateurService } from 'src/app/shared/utilisateur.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +9,34 @@ import { Component, OnInit } from '@angular/core';
   styles: []
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form: Utilisateur;
+  constructor(private service: UtilisateurService) { }
 
   ngOnInit() {
+    this.resetForm();
+  }
+
+  public soumettre(form:Form)
+  {
+    
+  }
+
+  public resetForm()
+  {
+    this.form = {
+      IDutilisateur: 0,
+      type: "",
+      nom: "",
+      prenom: "",
+      telephonetravail: "",
+      telephoneMaison: "",
+      telephoneMobile: "",
+      salaire: 0,
+      adresse: "",
+      email: "",
+      username: "",
+      password: ""
+    }
   }
 
 }
