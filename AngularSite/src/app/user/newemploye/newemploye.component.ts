b90c4c67bce7913c041d63af99fae391931ad815
Christian KABORE <christwkab@gmail.com>
2019-09-23 16:12:40 -0400

    Nouvel employe + Recevoir don

---
+++ AngularSite/src/app/user/newemploye/newemploye.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-newemploye',
+  templateUrl: './newemploye.component.html',
+  styles: []
+})
+export class NewemployeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
